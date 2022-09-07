4e399a39f9a1ee511f640726f2aecc6edd6a2e5f
Yogesh <yogesh.b@thoughtworks.com>
2022-09-07 08:07:31 +0530

    Yogesh | Test shell script in pipeline

--- secret_scan.sh
+++ secret_scan.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-TARGET_PATHS=("$@")
-for TARGET_PATH in "${TARGET_PATHS[@]}";
-do
-echo "Started Scanning $TARGET_PATH"
-trivy fs --exit-code 1 $TARGET_PATH
-echo "Completed Scanning $TARGET_PATH"
-done
+echo "Welcome Yogesh"
+# TARGET_PATHS=("$@")
+# for TARGET_PATH in "${TARGET_PATHS[@]}";
+# do
+# echo "Started Scanning $TARGET_PATH"
+# trivy fs --exit-code 1 $TARGET_PATH
+# echo "Completed Scanning $TARGET_PATH"
+# done
