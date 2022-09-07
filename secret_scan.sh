e4cf44b32c5312aa99bf899738ae65e100a33695
Yogesh <yogesh.b@thoughtworks.com>
2022-09-07 09:57:36 +0530

    Yogesh | Test shell script in pipeline

--- secret_scan.sh
+++ secret_scan.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 echo "Welcome Yogesh"
-TARGET_PATHS=("$@")
-if [ -z "$TARGET_PATHS" ]; then
-    TARGET_PATHS=(".")
-fi
-for TARGET_PATH in "${TARGET_PATHS[@]}";
+declare -a dirs
+i=1
+for d in */
 do
-echo "Started Scanning $TARGET_PATH"
-trivy fs --exit-code 1 $TARGET_PATH
-echo "Completed Scanning $TARGET_PATH"
+    dirs[i++]="${d%/}"
 done
+echo "There are ${#dirs[@]} dirs in the current path"
+for((i=1;i<=${#dirs[@]};i++))
+do
+    echo $i "${dirs[i]}"
+done
+# TARGET_PATHS=("$@")
+# if [ -z "$TARGET_PATHS" ]; then
+#     TARGET_PATHS=(".")
+# fi
+# for TARGET_PATH in "${TARGET_PATHS[@]}";
+# do
+# echo "Started Scanning $TARGET_PATH"
+# trivy fs --exit-code 1 $TARGET_PATH
+# echo "Completed Scanning $TARGET_PATH"
+# done
