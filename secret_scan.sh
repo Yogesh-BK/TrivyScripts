58c2d410a5668afcf4e739ef757f8a7e11f1ca12
Yogesh <yogesh.b@thoughtworks.com>
2022-09-08 12:40:23 +0530

    Yogesh | Test shell script in pipeline

--- secret_scan.sh
+++ secret_scan.sh
@@ -21,6 +21,6 @@ fi
 for TARGET_PATH in "${TARGET_PATHS[@]}";
 do
 echo "Started Scanning $TARGET_PATH"
-trivy fs --exit-code 1 $TARGET_PATH
+trivy fs --exit-code 1 "$TARGET_PATH"
 echo "Completed Scanning $TARGET_PATH"
 done
